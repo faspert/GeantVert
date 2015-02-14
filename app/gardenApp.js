0f7c920d7db863fcd97ba87ed1ede14472152afe
francois.aspert <francois.aspert@gmail.com>
2015-02-14 16:57:45 +0100

    First commit. Let's grow stuff.

---
+++ app/gardenApp.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+/**
+ * Déclaration de l'application gardenApp
+ */
+var gardenApp = angular.module('gardenApp', [
+    // Dépendances du "module"
+]);
+
+/**
+ * Contrôleur de l'application".
+ */
+//gardenApp.controller('MyController',['$scope'],function)
+
